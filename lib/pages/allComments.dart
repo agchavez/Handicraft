68242f9a6bf4ae8cdeb9e0fa8d1595975f50b17d
Angel Chavez <agchavez@unah.hn>
2021-08-17 00:34:48 -0600

    carousel ui

--- lib/pages/allComments.dart
+++ lib/pages/allComments.dart
@@ -39,12 +39,8 @@ class _AllCommentsState extends State<AllComments> {
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.black,
-          bottomOpacity: 0.0,
           elevation: 0.0,
-          actions: [],
-          title: Center(
-            child: Text('Comentarios'),
-          )),
+          title: Text('Comentarios')),
       body: SingleChildScrollView(child: _showComentary(size, data, context)),
       floatingActionButton: _comentary(size),
     );
